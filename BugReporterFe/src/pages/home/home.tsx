edc182b3d0d9041fc9acb4ed7bfcdd0e77595c92
Ben-Lane <benjaminkieranlane@gmail.com>
2026-09-06 16:02:49 +0100

    feat: Setup Styling Libraries
    ShadcnUI + TailwindCSS

--- BugReporterFe/src/pages/home/home.tsx
+++ BugReporterFe/src/pages/home/home.tsx
@@ -1,7 +1,14 @@
+import { Button } from "../../components/ui/button";
 export default function Home() {
   return (
     <>
-      <h1>HOME</h1>
+      <h1>Bug Report Wizard</h1>
+      <p>A tool used to track bugs within programming projects.</p>
+      <img className="w-[100px] h-[100px] border-2"></img>
+      <div>
+        <Button>Login</Button>
+        <Button>Signup</Button>
+      </div>
     </>
   );
 }
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # BugReporter-Fe
 Bug Report Wizard frontend client repository.
+
+# Used Technology
+- Typescript
+- React
+- Vite
+- React Router
+- CSS Tailwind
+- shadcnUI
+
+# Credits
+- 
