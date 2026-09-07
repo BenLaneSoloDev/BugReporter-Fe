7f0bbbf703f91af57d6a8395204b42248cfe89d9
Ben-Lane <benjaminkieranlane@gmail.com>
2026-09-07 09:29:56 +0100

    feat: Created Base Login Page

--- BugReporterFe/src/pages/login/login.tsx
+++ BugReporterFe/src/pages/login/login.tsx
@@ -1,7 +1,49 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+
 export default function Login() {
   return (
-    <>
-      <h1>LOGIN</h1>
-    </>
+    <div className="flex justify-center h-dvh items-center mx-5">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <div className="flex flex-row justify-between items-end">
+            <CardTitle>Login</CardTitle>
+            <CardAction>
+              <Button variant="outline">Sign Up</Button>
+            </CardAction>
+          </div>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-3">
+              <div className="grid gap-2">
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="Email"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <Input id="password" type="password" placeholder="Password" required />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button type="submit" className="w-full">
+            Login
+          </Button> 
+        </CardFooter>
+      </Card>
+    </div>
   );
 }
--- BugReporterFe/src/pages/signup/signup.tsx
+++ BugReporterFe/src/pages/signup/signup.tsx
@@ -3,14 +3,11 @@ import {
   Card,
   CardAction,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-
 
 export default function Signup() {
   return (
