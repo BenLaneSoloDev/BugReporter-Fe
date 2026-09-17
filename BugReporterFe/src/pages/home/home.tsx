95e6ba90e98003bef2e1e9a26b6d998848798847
Ben-Lane <benjaminkieranlane@gmail.com>
2026-09-17 12:54:30 +0100

    feat: Adjusted Home Page Based on Login Status

--- BugReporterFe/src/pages/home/home.tsx
+++ BugReporterFe/src/pages/home/home.tsx
@@ -1,3 +1,4 @@
+import isLoggedIn from "@/utility/isLoggedIn";
 import { Button } from "../../components/ui/button";
 
 import { useNavigate } from "react-router-dom";
@@ -5,18 +6,21 @@ import { useNavigate } from "react-router-dom";
 export default function Home() {
   
   let navigate = useNavigate();
-  
+  const loggedIn = isLoggedIn();
+
   return (
     <div className="flex justify-center items-center h-dvh">
       <div className="flex flex-col items-center gap-5">
         <h1 className="font-bold text-5xl uppercase">Bug Reporter</h1>
         <p className="font-light text-2xl">A tool for tracking bugs within programming projects.</p>
         <img className="w-[400px] h-[200px] border-2 border-cc-neutral"></img>
-        <div className="flex flex-row gap-5">
-          <Button onClick={() => navigate("/login")}>Login</Button>
-          <Button onClick={() => navigate("/signup")}>Signup</Button>
-        </div>
-        <Button onClick={() => navigate("/dashboard")}>Dashboard (dev)</Button>
+        { !loggedIn && (
+          <div className="flex flex-row gap-5">
+            <Button onClick={() => navigate("/login")}>Login</Button>
+            <Button onClick={() => navigate("/signup")}>Signup</Button>
+          </div>
+        )}
+        { loggedIn && <Button onClick={() => navigate("/dashboard")}>Dashboard</Button> }
       </div>
     </div>
   );
