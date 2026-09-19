011064ebc44fcd02f9f260ba30147b69e8941a0e
Ben-Lane <benjaminkieranlane@gmail.com>
2026-09-19 14:29:16 +0100

    feat: 404 Page Setup

--- BugReporterFe/src/pages/404/notFound.tsx
+++ BugReporterFe/src/pages/404/notFound.tsx
@@ -1,7 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
 export default function NotFound() {
+  
+  const navigate = useNavigate();
+  
   return (
-    <>
-      <h1>Error 404: Not Found</h1>
-    </>
+    <div className="flex flex-col justify-center items-center w-full h-dvh p-10">
+      <a onClick={() => navigate("/")} className="uppercase font-bold border-b-2 cursor-pointer hover:text-cc-green-3 transition-colors duration-200">Bug Reporter</a>
+      <p className="mt-4 font-mono text-center"><strong className="text-cc-green-2">Error 404:</strong> Page Not Found</p>
+    </div>
   );
 }
