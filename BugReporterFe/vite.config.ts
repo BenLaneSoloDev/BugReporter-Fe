d5a11b0dd0c06169b6d94484c686b4286970137c
Ben-Lane <benjaminkieranlane@gmail.com>
2026-09-21 10:45:39 +0100

    fix: Updated Vite Env Import

--- BugReporterFe/vite.config.ts
+++ BugReporterFe/vite.config.ts
@@ -1,16 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path';
 
-export default defineConfig({
-  plugins: [
-    react(), 
-    tailwindcss()
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(import.meta.dirname, "./src") 
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  // This will print directly inside Vercel's Build Logs during compilation
+  console.log('>>> VERCEL BUILD ENV CHECK:', env.VITE_API_KEY ? 'FOUND' : 'MISSING');
+
+  return {
+    plugins: [
+      react(), 
+      tailwindcss()
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(import.meta.dirname, "./src") 
+      }
     }
   }
-})
+});
